0f032b8a5ba57974327943b048e25768446f4cc3
TuncaT <43730654+evinai@users.noreply.github.com>
2023-10-20 10:59:41 +0000

    adding pdb debugger example

--- hello.py
+++ hello.py
@@ -13,3 +13,24 @@ def add_this(x, y):
 
 
 print(add_this("4", 3))
+#print(add_this("one", 3))
+
+
+# pdb
+def add_this(x, y):
+    # test with print
+    # import pdb;pdb.set_trace()
+    print(f"This is x: {x} and the x-type {type(x)}")
+    print(f"This is y: {y} and the y-type is {type(y)}")
+    try:
+        result = x + y
+    except TypeError:
+        print(f"The wrong type passed")
+        result = int(x) + int(y)
+        
+    print(f"This is the result: {result}")
+    return result
+
+
+print(add_this("4", 3))
+#print(add_this("one", 3))
